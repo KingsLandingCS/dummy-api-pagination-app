a514ab370a5b5b3500473201ac125113b4f37539
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-05-08 02:33:43 +0500

    learning

--- src/app/shared/search-pipe.pipe.ts
+++ src/app/shared/search-pipe.pipe.ts
@@ -5,13 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipePipe implements PipeTransform {
 
-  transform(arrayOfObjs: any[], inputData: any): any {
-    try {
-      const searchItem = inputData.toLowerCase();
-      return arrayOfObjs.filter((items: any) =>
-        items.title.includes(searchItem));
-    } catch (error) {
-      console.log(error);
-    }
+  transform(arrayOfObjs: any[], inputData: any): any[] {
+    const searchItem = inputData.toLowerCase();
+    return arrayOfObjs.filter
+      ((item: any) => item.title.toLowerCase().includes(searchItem) ||
+        item.brand.toLowerCase().includes(searchItem) ||
+        item.category.toLowerCase().includes(searchItem));
   }
 }
